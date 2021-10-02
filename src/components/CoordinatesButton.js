fa0580444090b2d436ee23867512395e84de1204
jamescross813 <crossjames813@gmail.com>
2021-10-02 12:42:56 -0500

    commit tree 2b5df00ea6e0be745b4a149cbd92e9979dc80805

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+class CoordinatesButton extends React.Component{
+
+    handleClick = (event) =>{
+        let coordinateArray = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(coordinateArray)
+    }
+
+    render(){
+        return <button onClick={this.handleClick}>Coordinate Button</button>
+    }
+
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+class DelayedButton extends React.Component{
+
+
+    handleClick = (event) =>{
+        const delayedEvent = event
+        this.props.onDelayedClick(event)
+    }
+
+    render(){
+        return <button onClick={this.handleClick(event.persist())}>Delay Click</button>
+    }
+}
+
+
+export default DelayedButton
